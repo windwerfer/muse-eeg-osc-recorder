0428927c942a8636ff2b646584d6066e7cd3fa0a
watdo <watdoichedimaemaem@gmail.com>
2024-09-05 12:31:50 +0700

    reactivated keyboard input_handler.py

--- lib/input_handler.py
+++ lib/input_handler.py
@@ -2,16 +2,17 @@
 import os
 import sys
 
+from lib.record_to_file import gracefully_end
 
 
 def start_input(data):
     # time.sleep(2)
     while True:
-        user_input = input("  --> Exit: x (+Enter) | note: n | s = stats       \n")
+        user_input = input("  --> Exit: x (+Enter)  \n")  # todo: | note: n | s = stats
         sys.stdout.flush()
 
         if user_input == 'x':
-            #gracefully_end()
+            gracefully_end(data)
             os._exit(0)
 
         sys.stdout.write(f"\rYou typed: {user_input}                      \n")
